ebe8cef8496149c5779aa674c5ed1a63f6b43933
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-07-17 11:01:14 -0700

    menu button

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Portfolio from "./components/portfolio/Portfolio"
 import Project from "./components/project/Project"
 import Contact from "./components/contact/Contact"
 import "./app.scss"
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
 
       <div className="app">
-        <Topbar/>
+        <Topbar menuOpen={menuOpen} 
+                setMenuOpen={setMenuOpen}/>
         <div className="sections">
           <Intro/>
           <Portfolio/>
