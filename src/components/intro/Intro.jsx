8e9e74ae1372ba47ff15e2278f04f5ec73917b09
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-07-18 12:33:44 -0700

    centered div

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,16 @@
 import "./intro.scss"
 
 
-export default function Intro({menuOpen}) {
+export default function Intro({ menuOpen }) {
   return (
-    <div id="intro" className="intro "></div>
+    <div id="intro" className="intro">
+      <div className="top"></div>
+      <div className="bottom">
+        <div className="imgContainer">
+          <img src="assets/pic1.jpg" alt=""></img>
+        </div>
+      </div>
+    </div>
   )
 }
 
