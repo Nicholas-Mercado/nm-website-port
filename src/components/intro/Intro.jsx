dbed2def41051192cd172a999833bb72526258e9
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-07-18 16:00:27 -0700

    added typewriter

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,14 +1,29 @@
 import "./intro.scss"
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import Typewriter from 'typewriter-effect';
 
-export default function Intro({ menuOpen }) {
+export default function Intro( ) {
+
+  new Typewriter('#typewriter', {
+    strings: ['Hello', 'World'],
+    autoStart: true,
+  });
+  
+
   return (
     <div id="intro" className="intro">
       <div className="top">
         <div className="wrapper">
           <h2> Hi! I'm</h2>
           <h1> Nicholas Mercado</h1>
-          <h3> placeholder<span></span></h3>
+          <h2 ><Typewriter 
+           options={{ 
+           strings:["Developer", "Coder", "Problem Solver", "Photographer"],
+           autoStart:true,
+           delay:75,
+           loop:true
+           }}
+           /> </h2>
 
         </div>
         <a href="#portfolio">
