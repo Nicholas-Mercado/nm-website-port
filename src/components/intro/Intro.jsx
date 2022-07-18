a5193ac006a8a99f2295d77db4b5b12c0e1b3ddb
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-07-18 14:47:38 -0700

    added style

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,13 +1,25 @@
 import "./intro.scss"
-
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 
 export default function Intro({ menuOpen }) {
   return (
     <div id="intro" className="intro">
-      <div className="top"></div>
+      <div className="top">
+        <div className="wrapper">
+          <h2> Hi! I'm</h2>
+          <h1> Nicholas Mercado</h1>
+          <h3> placeholder<span></span></h3>
+
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDownRoundedIcon 
+            style={{ color: 'black',fontSize: 60 }}
+          />
+        </a>
+      </div>
       <div className="bottom">
         <div className="imgContainer">
-          <img src="assets/pic1.jpg" alt=""></img>
+          <img src="assets/test2.png" alt=""></img>
         </div>
       </div>
     </div>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,21 +1,23 @@
 import "./topbar.scss"
-import PhoneAndroidOutlinedIcon from '@mui/icons-material/PhoneAndroidOutlined';
-import MailRoundedIcon from '@mui/icons-material/MailRounded';
+// import PhoneAndroidOutlinedIcon from '@mui/icons-material/PhoneAndroidOutlined';
+// import MailRoundedIcon from '@mui/icons-material/MailRounded';
 
 export default function Topbar({menuOpen, toggleMenu}) {
   return (
     <div className={"topbar " + (menuOpen && "active ")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo" >
-            Nicholas Mercado
-          </a>
-          <div className="itemContainer">
+          
+            <a href="#intro" className="logo" >
+              Nicholas Mercado
+            </a>
+          
+          {/* <div className="itemContainer">
             <PhoneAndroidOutlinedIcon className="icon "/>
             <span className="iconRight">+555 555 5555</span>
             <MailRoundedIcon className="icon"/>
             <span>Nicholasamerc@gmail.com</span>
-          </div>
+          </div> */}
         </div>
 
         <div className="right">
