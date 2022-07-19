da1bf3cfc2607a115a90a89172bfa36c34e1f6cb
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-07-18 21:27:08 -0700

    added data for swiper

---
+++ src/components/portfolio/data.js
@@ -0,0 +1,46 @@
+const dataN = [
+    {
+        "id": 1,
+        "projectName": "Tech-Spotlight",
+        "img": "assets/pic1.jpg",
+        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
+    
+    },
+    {
+        "id": 2,
+        "projectName": "Tech-Spotlight",
+        "img": "./pic1.jpg",
+        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
+    
+    },
+    {
+        "id": 3,
+        "projectName": "Tech-Spotlight",
+        "img": "./pic1.jpg",
+        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
+    
+    },
+    {
+        "id": 4,
+        "projectName": "Tech-Spotlight",
+        "img": "./pic1.jpg",
+        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
+    
+    },
+    {
+        "id": 5,
+        "projectName": "Tech-Spotlight",
+        "img": "./pic1.jpg",
+        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
+    
+    },
+    {
+        "id": 6,
+        "projectName": "Tech-Spotlight",
+        "img": "./pic1.jpg",
+        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
+    
+    },
+]
+
+export default dataN
