9dfd04d6b2fed8e23db97a05d74e80c5c14fcbec
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-07-20 16:53:59 -0700

    updated data

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -2,7 +2,7 @@ const dataN = [
     {
         "id": 1,
         "projectName": "Tech-Spotlight",
-        "img": "assets/pic1.jpg",
+        "img": "assets/tech-spot.png",
         "GitHub":"https://github.com/regex-rejects/tech-spotlight" ,
         "Jupyter": "https://www.kaggle.com/code/edenbrekke/tech-spotlight-indeed-data-18may2022/notebook",
         "description": "Tech Spotlight scrapes Indeed.com and returns frequency of technologies found in all postings scraped. It uses the search queries,job title, location, date of post and returns a file of most popular technologies. We plotted our findings in a Jupyter Notebook."
@@ -10,41 +10,24 @@ const dataN = [
     },
     {
         "id": 2,
-        "projectName": "Check?",
-        "img": "./pic1.jpg",
-        "GitHub":"https://github.com/regex-rejects/tech-spotlight" ,
+        "projectName": "Do or Doom",
+        "img": "assets/dodoom.png",
+        "GitHub":"https://github.com/TheG0ATS/do-or-doom-fe" ,
         "Jupyter": "",
-        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
+        "description": "A task list application that allows a user to create tasks,set a due date, and add an accountability partner. If that task is not completed by the due date a text message is sent to your accountability partner. This application uses a next.js frontend and a Django backend with an ElephantSQL database."
     
     },
     {
         "id": 3,
-        "projectName": "Tech-Spotlight",
+        "projectName": "Animal Racer",
         "img": "./pic1.jpg",
+        "GitHub":"https://github.com/The-Go-Gitters/trivia-race" ,
+        "Jupyter": "",
         "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
     
     },
-    {
-        "id": 4,
-        "projectName": "Tech-Spotlight",
-        "img": "./pic1.jpg",
-        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
     
-    },
-    {
-        "id": 5,
-        "projectName": "Tech-Spotlight",
-        "img": "./pic1.jpg",
-        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
     
-    },
-    {
-        "id": 6,
-        "projectName": "Tech-Spotlight",
-        "img": "./pic1.jpg",
-        "description": "loren ipsum, loren ipsum, loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum,loren ipsum, loren ipsum"
-    
-    },
 ]
 
 export default dataN
