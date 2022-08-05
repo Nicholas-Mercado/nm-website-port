e09d2701c9252636b95134df1f2b44d2d62d07d6
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-08-05 13:33:05 -0700

    changed copy

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -5,7 +5,7 @@ const dataN = [
         "img": "assets/tech-spot.png",
         "GitHub":"https://github.com/regex-rejects/tech-spotlight" ,
         "Jupyter": "https://www.kaggle.com/code/edenbrekke/tech-spotlight-indeed-data-18may2022/notebook",
-        "description": "Tech Spotlight scrapes Indeed.com and returns frequency of technologies found in all postings scraped. It uses the search queries,job title, location, date of post and returns a file of most popular technologies. We plotted our findings in a Jupyter Notebook."
+        "description": "Tech Spotlight scrapes Indeed.com and returns the frequency of technologies found in all postings scraped. It uses search queries, job title, location, and date of post and returns a file of the most popular technologies. We plotted our findings in a Jupyter Notebook."
     
     },
     {
