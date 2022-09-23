801861ab2401162c8641cddb288404cd07d16581
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-09-22 17:30:28 -0700

    can flip cards

--- src/components/portfolio/Portfolion.jsx
+++ src/components/portfolio/Portfolion.jsx
@@ -1,29 +1,33 @@
 import "./portfolion.scss";
 import data from './data.js';
 
-
-
-
 export default function Portfolion() {
-
-  
   return (
-    <>
-        <div className="portfolio">
-            <div className="cardholder">
-                {data.map((data, index) => ( 
-                    <div  >
-                        <div  className="card">
-                            <div className="card-size">
-                                <img src={data.img} alt="Nature" /> 
-                                <h3>{data.projectName}</h3>
-                                <p>{data.description}</p>
-                            </div>
+<>                 
+    <div className="scene portfolio">
+    {data.map((data, index) => ( 
+        <div className="container">
+            <div className="row flip-boxes">
+                <div className="col-md-3 col-sm-4 col-8 flip-box">
+                    <div className="front" >
+                        <img src={data.img} alt="project img"></img>
+                        <div className="content text-center">
+                        <h2>{data.projectName}</h2>
+                        <span className="click-for-more">
+                        </span>
                         </div>
                     </div>
-                    ))}
+                <div className="back">
+                    <div className="content">
+                    First Back
+                    </div>
+                </div>
+                </div>
+    
             </div>
-        </div> 
-    </>
-  );
+        </div>
+    ))}
+    </div>
+</>
+);
 }
