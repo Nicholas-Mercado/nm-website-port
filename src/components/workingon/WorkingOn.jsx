e755ece05b86ee0683d88c068d2e793905651c0b
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-10-04 21:09:34 -0700

    added more style

--- src/components/workingon/WorkingOn.jsx
+++ src/components/workingon/WorkingOn.jsx
@@ -12,12 +12,13 @@ export default function WorkingOn() {
                   <div className="details">
                     <h3>DND-Bot</h3>
                     <p> Stuff and details about the dnd bot</p>
+                    <div className="youtube">
+                  <iframe width="460" height="255" src="https://www.youtube.com/embed/xc1VpbRd4is" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
                   </div>
                 </div>
                 <div className="right">
-                  <div className="youtube">
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/xc1VpbRd4is" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  </div>
+                  
                   <div className="other">
                     <h3>Currently studying</h3>
                     <p>I am studying to take my Cloud practitioner exam from AWS.</p>
