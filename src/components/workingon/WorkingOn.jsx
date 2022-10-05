7320b2ec13d385169f764730d9e5777c7bdbaaae
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-10-04 17:57:00 -0700

    styliing and divs

--- src/components/workingon/WorkingOn.jsx
+++ src/components/workingon/WorkingOn.jsx
@@ -3,8 +3,28 @@ import "./workingon.scss"
 export default function WorkingOn() {
   return (
     <>
-        <div className="workingon" id="workingon"></div>
-        <h2>What I am Working on!</h2>
+        <div className="workingon" id="workingon">
+          
+            <div className="container">
+                <h2>What I am Working on!</h2>
+              <div className="card">
+                <div className="left">
+                  <div className="details">
+                    <h3>DND-Bot</h3>
+                    <p> Stuff and details about the dnd bot</p>
+                  </div>
+                </div>
+                <div className="right">
+                  <div className="youtube">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/xc1VpbRd4is" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+                  <div className="imgs"></div>
+                </div>
+
+              </div>
+          </div>  
+          
+        </div>
     </>
   )
 }
