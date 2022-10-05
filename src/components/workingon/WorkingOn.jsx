cda47969882450e6c68539bc8217de83adb3a7ac
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-10-04 17:15:00 -0700

    created working on page,scss

---
+++ src/components/workingon/WorkingOn.jsx
@@ -0,0 +1,10 @@
+import "./workingon.scss"
+
+export default function WorkingOn() {
+  return (
+    <>
+        <div className="workingon" id="workingon"></div>
+        <h2>What I am Working on!</h2>
+    </>
+  )
+}
