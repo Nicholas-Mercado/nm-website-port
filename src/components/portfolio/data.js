a2186fb37a3f50e7cddb950d200471c57ff616dd
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-10-05 17:45:58 -0700

    added new images

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -11,7 +11,7 @@ const dataN = [
     {
         "id": 2,
         "projectName": "Do or Doom",
-        "img": "assets/dodoom.pn",
+        "img": "assets/do_or_doom.JPG",
         "GitHub":"https://github.com/TheG0ATS/do-or-doom-fe" ,
         "Jupyter": "",
         "description": "A task list application that allows a user to create tasks, set a due date, and add an accountability partner. If that task is not completed by the due date a text message is sent to your accountability partner. This application uses a React-next.js frontend and a Django backend with an ElephantSQL database."
@@ -20,7 +20,7 @@ const dataN = [
     {
         "id": 3,
         "projectName": "Animal Racer",
-        "img": "assets/racer.pn",
+        "img": "assets/AR.JPG",
         "GitHub":"https://github.com/The-Go-Gitters/trivia-race" ,
         "Jupyter": "",
         "description": "Animal Racer is a web-based trivia game built in JavaScript. Each turn has a new animal trivia question and if you answer correctly, you move closer to the finish line. Your opponent, the computer, races you around the track and has a probability of answering correctly."
@@ -29,7 +29,7 @@ const dataN = [
     {
         "id": 4,
         "projectName": "This Site!",
-        "img": "assets/portweb.png",
+        "img": "assets/thissite.JPG",
         "GitHub":"https://github.com/Nicholas-Mercado/nm-website-port" ,
         "Jupyter": "",
         "description": "This site is built with React and Material UI and then styled with SASS using the SCSS syntax. It is hosted on AWS. It offers viewers a chance to meet me and see the projects I have been working on over the last few months."
